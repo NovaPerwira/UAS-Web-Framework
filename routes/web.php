7c7d45d54284dc7e1afc945544b627c637547c48
novaperwira <novaperwira30@gmail.com>
2026-01-21 16:44:45 +0800

    update nav ui func

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\FreelancerController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
 Route::resource('projects', ProjectController::class);
 Route::resource('clients', ClientController::class);
 Route::resource('freelancers', FreelancerController::class);
